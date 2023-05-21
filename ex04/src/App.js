ce34355b034a95a10a37f941e569f48f721bc575
frontjsw <89008053+frontjsw@users.noreply.github.com>
2023-05-22 00:03:57 +0900

    add ex04

---
+++ ex04/src/App.js
@@ -0,0 +1,17 @@
+import react from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
+}
+
+export default App;
